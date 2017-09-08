542b05a6100ca974ea04c308b75acb2fd2b2d1de
keith(mac) <ka33145@googlemail.com>
2017-09-08 12:11:30 +0100

    Add test for top_up method for oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-  it "initialized card will have a balance of 0" do
+  it 'initialized card will have a balance of 0' do
     expect(subject.balance).to eq(0)
+  end
+
+  it 'Top up balance' do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
   end
 end
