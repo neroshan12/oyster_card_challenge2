79e59e4af632f769fb9241deb00f9dfd9af7eb65
keith(mac) <ka33145@googlemail.com>
2017-09-08 16:00:23 +0100

    Refactored to lib/oystercard.rb to take station name on touch out and touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,18 @@
 class Oystercard
-  attr_reader :balance , :in_journey
-  MAXIMUM_BALANCE = 90
+  attr_reader :balance, :in_journey, :entry_station
+  MAX_BALANCE = 90
   MIN_FEE = 1
-  MINIMUM_FARE = 3
+  MIN_FARE = 3
 
   def initialize
     @balance = 0
     @in_journey = false
+
   end
 
   def top_up(value)
-    err_msg = "Sorry, you have exceeded the maximum balance (£#{MAXIMUM_BALANCE})"
-    raise err_msg if @balance + value > MAXIMUM_BALANCE
+    err_msg = "Sorry, you have exceeded the maximum balance (£#{MAX_BALANCE})"
+    raise err_msg if @balance + value > MAX_BALANCE
     @balance += value
   end
 
@@ -20,17 +21,20 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    !!@entry_station
   end
 
-  def touch_in
+  def touch_in(entry_station)
     err_msg = 'Not enough funds for travel'
     raise err_msg if @balance < MIN_FEE
     @in_journey = true
+    @entry_station = entry_station
   end
 
-  def touch_out
+  def touch_out(exit_station)
     @in_journey = false
-    deduct(MINIMUM_FARE)
+    deduct(MIN_FARE)
+    @entry_station = nil
+    @exit_station = exit_station
   end
 end
